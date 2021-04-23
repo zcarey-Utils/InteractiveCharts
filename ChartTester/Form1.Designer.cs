ae17937e4bbc611e487a7a9fef9f0094c4ae2eac
zcareyltu <38868466+zcareyltu@users.noreply.github.com>
2021-04-23 10:01:56 -0400

    Improved ChartTester

--- ChartTester/Form1.Designer.cs
+++ ChartTester/Form1.Designer.cs
@@ -24,34 +24,108 @@ namespace ChartTester {
 		///  the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.betterSunburst1 = new InteractiveCharts.BetterSunburst();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.SunburstMenu = new System.Windows.Forms.ToolStripMenuItem();
+			this.BetterSunburst = new System.Windows.Forms.ToolStripMenuItem();
+			this.Sunburst = new System.Windows.Forms.ToolStripMenuItem();
+			this.VasturianoSunburst = new System.Windows.Forms.ToolStripMenuItem();
+			this.ZoomableSunburst = new System.Windows.Forms.ToolStripMenuItem();
+			this.LimitedSunburst = new System.Windows.Forms.ToolStripMenuItem();
+			this.ChartPanel = new System.Windows.Forms.Panel();
+			this.menuStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
-			// betterSunburst1
+			// menuStrip1
 			// 
-			this.betterSunburst1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.betterSunburst1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.betterSunburst1.Location = new System.Drawing.Point(0, 0);
-			this.betterSunburst1.Name = "betterSunburst1";
-			this.betterSunburst1.Size = new System.Drawing.Size(1090, 739);
-			this.betterSunburst1.TabIndex = 0;
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SunburstMenu});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(1090, 24);
+			this.menuStrip1.TabIndex = 1;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// SunburstMenu
+			// 
+			this.SunburstMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BetterSunburst,
+            this.Sunburst,
+            this.VasturianoSunburst,
+            this.ZoomableSunburst,
+            this.LimitedSunburst});
+			this.SunburstMenu.Name = "SunburstMenu";
+			this.SunburstMenu.Size = new System.Drawing.Size(66, 20);
+			this.SunburstMenu.Text = "Sunburst";
+			// 
+			// BetterSunburst
+			// 
+			this.BetterSunburst.Name = "BetterSunburst";
+			this.BetterSunburst.Size = new System.Drawing.Size(176, 22);
+			this.BetterSunburst.Text = "BetterSunburst";
+			this.BetterSunburst.Click += new System.EventHandler(this.BetterSunburst_Click);
+			// 
+			// Sunburst
+			// 
+			this.Sunburst.Name = "Sunburst";
+			this.Sunburst.Size = new System.Drawing.Size(176, 22);
+			this.Sunburst.Text = "Sunburst";
+			this.Sunburst.Click += new System.EventHandler(this.Sunburst_Click);
+			// 
+			// VasturianoSunburst
+			// 
+			this.VasturianoSunburst.Name = "VasturianoSunburst";
+			this.VasturianoSunburst.Size = new System.Drawing.Size(176, 22);
+			this.VasturianoSunburst.Text = "VasturianoSunburst";
+			this.VasturianoSunburst.Click += new System.EventHandler(this.VasturianoSunburst_Click);
+			// 
+			// ZoomableSunburst
+			// 
+			this.ZoomableSunburst.Name = "ZoomableSunburst";
+			this.ZoomableSunburst.Size = new System.Drawing.Size(176, 22);
+			this.ZoomableSunburst.Text = "ZoomableSunburst";
+			this.ZoomableSunburst.Click += new System.EventHandler(this.ZoomableSunburst_Click);
+			// 
+			// LimitedSunburst
+			// 
+			this.LimitedSunburst.Name = "LimitedSunburst";
+			this.LimitedSunburst.Size = new System.Drawing.Size(176, 22);
+			this.LimitedSunburst.Text = "LimitedSunburst";
+			this.LimitedSunburst.Click += new System.EventHandler(this.LimitedSunburst_Click);
+			// 
+			// ChartPanel
+			// 
+			this.ChartPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.ChartPanel.Location = new System.Drawing.Point(0, 24);
+			this.ChartPanel.Name = "ChartPanel";
+			this.ChartPanel.Size = new System.Drawing.Size(1090, 715);
+			this.ChartPanel.TabIndex = 2;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(1090, 739);
-			this.Controls.Add(this.betterSunburst1);
+			this.Controls.Add(this.ChartPanel);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
 			this.Name = "Form1";
 			this.Text = "Form1";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
-
-		private InteractiveCharts.LimitedSunburst testSunburst1;
-		private InteractiveCharts.BetterSunburst betterSunburst1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem SunburstMenu;
+		private System.Windows.Forms.ToolStripMenuItem BetterSunburst;
+		private System.Windows.Forms.ToolStripMenuItem Sunburst;
+		private System.Windows.Forms.ToolStripMenuItem VasturianoSunburst;
+		private System.Windows.Forms.ToolStripMenuItem ZoomableSunburst;
+		private System.Windows.Forms.ToolStripMenuItem LimitedSunburst;
+		private System.Windows.Forms.Panel ChartPanel;
 	}
 }
 
--- ChartTester/Form1.cs
+++ ChartTester/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InteractiveCharts;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,9 +11,29 @@ using System.Windows.Forms;
 
 namespace ChartTester {
 	public partial class Form1 : Form {
+
+		private Chart chart;
+
 		public Form1() {
 			InitializeComponent();
 		}
 
+		private void LoadChart<T>() where T : Chart, new() {
+			if(chart != null) {
+				ChartPanel.Controls.Remove(chart);
+				chart = null;
+			}
+
+			chart = new T();
+			chart.Dock = DockStyle.Fill;
+			chart.Name = "Chart1";
+			ChartPanel.Controls.Add(chart);
+		}
+
+		private void BetterSunburst_Click(object sender, EventArgs e) => LoadChart<BetterSunburst>();
+		private void LimitedSunburst_Click(object sender, EventArgs e) => LoadChart<LimitedSunburst>();
+		private void Sunburst_Click(object sender, EventArgs e) => LoadChart<Sunburst>();
+		private void VasturianoSunburst_Click(object sender, EventArgs e) => LoadChart<VasturianoSunburst>();
+		private void ZoomableSunburst_Click(object sender, EventArgs e) => LoadChart<ZoomableSunburst>();
 	}
 }
